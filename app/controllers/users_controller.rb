10372b2eeb1d71ab50164b31726be2b6e22b02e6
bilalmdol <bilal.mohammad@ordwaylabs.com>
2023-08-22 16:57:57 +0530

    user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,20 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome to #{@user.username} the Alpha Blog!!"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:users).permit(:username, :email, :password)
+  end
 end
